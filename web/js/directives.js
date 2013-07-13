28992b2558983527c3cfe38485648434159adb08
rajasaur <rajasaur@gmail.com>
2013-07-13 22:08:23 +0530

    Add Validations.
    Added a Twig directive that includes "Required" error message when form_row is used. TODO: Make other kind of validations through a directive.
    
    Also, in the compileHtml directive, removed creating an isolated scope and share the scope as the parent.

--- web/js/directives.js
+++ web/js/directives.js
@@ -7,12 +7,11 @@ angular.module('commusoft.directives', []).
   directive('compileHtml', ['$compile', function($compile) {
     return {
         restrict: 'A',
-        scope: { compileHtml : '=' },
         replace: true,
-    
+
         link: function (scope, element, attrs) {
-            scope.$watch('compileHtml', function(value)  {
-                element.html ($compile(value)(scope.$parent));
+            scope.$watch(attrs.compileHtml, function(value)  {
+                element.html ($compile(value)(scope));
             });
         }
     };
